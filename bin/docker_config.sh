cb4e4832a176b8edac14afdd99e57de312e562f9
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2018-08-21 11:46:18 -0400

    Make docker_config.sh a little smarter about usage

--- bin/docker_config.sh
+++ bin/docker_config.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
 NETWORK_NAME=scratchapi_scratch_network
 
-if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
-     docker network create ${NETWORK_NAME} ; 
-fi
+command=$1
+shift
+arg=$1
+shift
+
+case "$command" in
+network)
+    if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
+        docker network create ${NETWORK_NAME} ; 
+    fi
+    ;;
+*)
+     echo "usage:"
+     echo "    docker_config.sh <command> <arg>"
+     echo ""
+     echo "  Currently only performs 'docker_config.sh network create'"
+     ;;
+esac
